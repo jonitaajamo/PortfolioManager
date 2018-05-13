ea6edc523302a02dcec4f8ac60a8b72a6e9dda4d
Joni Taajamo <joni.taajamo@outlook.com>
2018-05-13 19:25:35 +0300

    events can be fetched from api and rendered

---
+++ frontend/portfoliomanager-frontend/src/components/RenderEvents.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+class RenderEvents extends Component {
+
+    renderEvents() {
+        return this
+            .props
+            .eventData
+            .map((item, key) => {
+                const endYear = item.ended
+                    ? item.year + item.length
+                    : "";
+
+                const eventType = item.type === "work"
+                    ? "Workplace"
+                    : "School";
+
+                return (
+                    <div className="box has-text-left">
+                        <article>
+                            <div className="media-content">
+                                <p>
+                                    <strong>{item.name}</strong>
+                                    <small>
+                                        <br/> {item.year}
+                                        &nbsp;- {endYear},
+                                    </small>
+                                    <small>
+                                        &nbsp;&nbsp;{eventType}
+                                    </small>
+                                    <br/>{item.description}
+                                </p>
+                                <div className="has-text-right">
+                                    <button
+                                        className="button is-danger"
+                                        onClick={() => this.props.onEventDelete(item.id)}>X</button>
+                                </div>
+                            </div>
+
+                        </article>
+                    </div>
+                )
+            })
+    }
+
+    render() {
+        return (
+            <div className="column is-vertical is-half">
+                {this.renderEvents()}
+            </div>
+        );
+    }
+}
+
+export default RenderEvents;
